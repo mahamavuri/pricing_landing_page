549106df4f4cc77674285f53b126bd5d7a7700a6
mahamavuri <141739596+mahamavuri@users.noreply.github.com>
2024-06-19 17:05:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const priceElements = [
   let isUSD = true;
   const usdToInrRate = 83.10;
 
-  // update price after adding accessory
+  
 function updatePrice(element) {
     const basePrice = parseFloat(element.getAttribute('data-price')) || 0; // Default to 0 if attribute is not set
     const accessoryPrice = checkbox.checked ? 100 : 0;
@@ -39,12 +39,7 @@ function convertToINR() {
     isUSD = true;
     priceElements.forEach(element => updatePrice(element));
   }
-/*checkbox.addEventListener("click", () => {
-  basic.textContent = basic.textContent === "$0" ? "$0" : "$0";
-  professional.textContent =
-    professional.textContent === "$239.99" ? "$39.99 " : "$239.99";
-  master.textContent = master.textContent === "$399.99" ? "$99.99" : "$399.99";
-});*/
+
 checkbox.addEventListener("click", ()=>{
     priceElements.forEach(element => updatePrice(element));
 })
